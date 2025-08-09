74f73a81ccf7861c04ce35a480b039e90e9ea8d6
58Jamil58 <136060025+58Jamil58@users.noreply.github.com>
2025-08-10 00:09:01 +0530

    Initial commit

---
+++ BACKEND/app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { nanoid } from "nanoid";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import connectDB from "./src/config/monogo.config.js";
+import short_url from "./src/routes/short_url.route.js";
+import user_routes from "./src/routes/user.routes.js";
+import auth_routes from "./src/routes/auth.routes.js";
+import { redirectFromShortUrl } from "./src/controller/short_url.controller.js";
+import { errorHandler } from "./src/utils/errorHandler.js";
+import { attachUser } from "./src/utils/attachUser.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '.env') });
+
+if (!process.env.MONGO_URI) {
+  console.error("❌ ERROR: MONGO_URI is not set in .env file");
+  process.exit(1);
+}
+
+console.log("✅ Loaded URI:", process.env.MONGO_URI);
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173', // React frontend
+  credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(attachUser);
+
+app.use("/api/user", user_routes);
+app.use("/api/auth", auth_routes);
+app.use("/api/create", short_url);
+app.get("/:id", redirectFromShortUrl);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
